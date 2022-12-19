92c5401ee52d999dddb0e526887aeafca83c8dc9
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-19 14:21:12 +0300

    Create AppField.vue

---
+++ 4/src/components/AppField.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="form-group">
+      <label>
+         {{ this.label }}
+         <span v-if="this.activated"
+               :class="this.valid ? 'fa fa-check-circle text-success' : 'fa fa-exclamation-circle text-danger'"
+         ></span>
+      </label>
+
+      <input type="text" class="form-control" :value="this.value" @input="onInput">
+   </div>
+</template>
+
+<script>
+export default {
+   props: {
+      label: { type: String, required: true },
+      value: { type: String, required: true },
+      activated: { type: Boolean, default: false },
+      valid: { type: Boolean, default: false },
+   },
+
+   methods: {
+      onInput(e) {
+         this.$emit('change-input', e.target.value);
+      },
+   },
+};
+</script>
+
+<style scoped>
+
+</style>
