97224f6cfa4bdc3dd45072b3c33dc4eb6320280f
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-20 15:47:51 +0300

    Update Field.vue

--- 4/src/components/Field.vue
+++ 4/src/components/Field.vue
@@ -2,15 +2,21 @@
    <div class="form-group">
       <label class="m-1">
          {{ label }}
-         <transition name="fade" v-if="activated" mode="out-in" appear appear-class="custom-appear-class">
-<!--            <span v-if="activated">-->
-<!--               <transition name="blink">-->
-                  <font-awesome-icon :icon="'circle-' + (valid ? 'check' : 'exclamation')"
-                                     :class="valid ? 'text-success' : 'text-danger'"
-                                     :key="valid"
-                  />
-<!--               </transition>-->
-<!--            </span>-->
+         <transition
+             name="fade"
+             v-if="activated"
+             mode="out-in"
+             appear
+             appear-active-class="first-appear-class">
+            <!-- appear - анимация при первой загрузке анимации -->
+            <!-- appear-active-class - нужен для указания класса -->
+            <!-- out-in - Сначала исчезает старый элемент, и только после этого появляется новый -->
+
+            <font-awesome-icon :icon="'circle-' + (valid ? 'check' : 'exclamation')"
+                               :class="valid ? 'text-success' : 'text-danger'"
+                               :key="valid" />
+            <!-- key - нужен для уникальности-->
+
          </transition>
       </label>
 
@@ -39,17 +45,39 @@ export default {
 .fade-enter-active {
    animation: iconIn .5s;
 }
+
 .fade-leave-active {
    animation: iconOut .5s;
 }
 
+.first-appear-class {
+   animation: firstIn .8s;
+}
+
 @keyframes iconIn {
-   from{ transform: rotateY(-90deg); }
-   to{ transform: rotateY(0); }
+   from {
+      transform: rotateY(-90deg);
+   }
+   to {
+      transform: rotateY(0);
+   }
 }
 
 @keyframes iconOut {
-   from{ transform: rotateY(0); }
-   to{ transform: rotateY(90deg); }
+   from {
+      transform: rotateY(0);
+   }
+   to {
+      transform: rotateY(90deg);
+   }
+}
+
+@keyframes firstIn {
+   from {
+      opacity: 0;
+   }
+   to {
+      opacity: 1;
+   }
 }
 </style>
