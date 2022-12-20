617c7e95f936ca22f6f8f486caa162432aa6ab44
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-20 14:22:16 +0300

    Update Field.vue

--- 4/src/components/Field.vue
+++ 4/src/components/Field.vue
@@ -2,10 +2,13 @@
    <div class="form-group">
       <label class="m-1">
          {{ label }}
-         <span v-if="activated">
-            <font-awesome-icon :icon="'circle-' + (valid ? 'check' : 'exclamation')"
-                               :class="valid ? 'text-success' : 'text-danger'" />
-         </span>
+         <transition name="fade">
+            <span v-if="activated">
+               <font-awesome-icon :icon="'circle-' + (valid ? 'check' : 'exclamation')"
+                                  :class="valid ? 'text-success' : 'text-danger'"
+               />
+            </span>
+         </transition>
       </label>
 
       <input type="text" class="form-control" :value="value" @input="onInput">
@@ -30,5 +33,27 @@ export default {
 </script>
 
 <style scoped>
+   .fade-enter-active {
+      animation: fadeIn 1s;
+   }
+   .fade-leave-active {
+      animation: fadeOut 1s;
+   }
 
+   @keyframes fadeIn {
+      from {
+         opacity: 0;
+      }
+      to {
+         opacity: 1;
+      }
+   }
+   @keyframes fadeOut {
+      from {
+         opacity: 1;
+      }
+      to {
+         opacity: 0;
+      }
+}
 </style>
