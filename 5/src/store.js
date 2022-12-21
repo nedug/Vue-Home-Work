66b1a1c21bd65d5e35bf7108f601cc90b4c35c62
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-21 14:28:10 +0300

    11

--- 5/src/store.js
+++ 5/src/store.js
@@ -1,4 +1,4 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 /*
 	const ORDER_STATUSES = {
@@ -9,47 +9,47 @@ import { createStore } from 'vuex'
 */
 
 const store = {
-	state: {
-		price: 1000,
-		cnt: 1,
-		orderStatus: 0
-	},
-	getters: {
-		price: state => state.price,
-		cnt: state => state.cnt,
-		total: state => state.price * state.cnt,
-		orderIsNothing: state => state.orderStatus == 0, // wtf my english
-		orderIsPending: state => state.orderStatus == 1,
-		orderIsDone: state => state.orderStatus == 2
-	},
-	mutations: {
-		decrease(state){
-			if(state.cnt > 1){
-				state.cnt--;
-			}
-		},
-		increase(state){
-			state.cnt++;
-		},
-		setCnt(state, val){
-			state.cnt = Math.max(1, val);
-		},
-		setOrderStatus(state, status){
-			state.orderStatus = status;
-		}
-	},
-	actions: {
-		send(store){
-			store.commit('setOrderStatus', 1);
+    state: {
+        price: 1000,
+        cnt: 1,
+        orderStatus: 0,
+    },
+    getters: {
+        price: state => state.price,
+        cnt: state => state.cnt,
+        total: state => state.price * state.cnt,
+        orderIsNothing: state => state.orderStatus == 0, // wtf my english
+        orderIsPending: state => state.orderStatus == 1,
+        orderIsDone: state => state.orderStatus == 2,
+    },
+    mutations: {
+        decrease(state) {
+            if (state.cnt > 1) {
+                state.cnt--;
+            }
+        },
+        increase(state) {
+            state.cnt++;
+        },
+        setCnt(state, val) {
+            state.cnt = Math.max(1, val);
+        },
+        setOrderStatus(state, status) {
+            state.orderStatus = status;
+        },
+    },
+    actions: {
+        send(store) {
+            store.commit('setOrderStatus', 1);
 
-			// request to server
-			setTimeout(() => {
-				store.commit('setOrderStatus', 2);
-			}, 500);
-		}
-	},
-	strict: process.env.NODE_ENV !== 'production'
-}
+            // request to server
+            setTimeout(() => {
+                store.commit('setOrderStatus', 2);
+            }, 500);
+        },
+    },
+    strict: process.env.NODE_ENV !== 'production',
+};
 
 export default createStore(store);
 
