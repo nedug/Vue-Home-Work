d2091cbb9e7f0d444f6eda0234e54ff61dbf5f3a
Alexander R <80103497+nedug@users.noreply.github.com>
2023-01-05 09:57:10 +0300

    Update products.js

--- 6/src/store/products.js
+++ 6/src/store/products.js
@@ -1,24 +1,20 @@
 export default {
-	namespaced: true,
-	state: {
-		items: stub()
-	},
-	getters: {
-		all: state => state.items
-	},
-	mutations: {
-		
-	},
-	actions: {
-		
-	}
-}
+    namespaced: true,
+    state: {
+        items: stub(),
+    },
+    getters: {
+        all: state => state.items,
+    },
+    mutations: {},
+    actions: {},
+};
 
-function stub(){
-	return [
-		{"id":100,"title":"Ipnone 200","price":12000,"rest":10},
-		{"id":101,"title":"Samsung AAZ8","price":22000,"rest":5},
-		{"id":103,"title":"Nokia 3310","price":5000,"rest":2},
-		{"id":105,"title":"Huawei ZZ","price":15000,"rest":8}
-	];
+function stub() {
+    return [
+        { 'id': 100, 'title': 'Iphone 14', 'price': 1200, 'rest': 10 },
+        { 'id': 101, 'title': 'Samsung AAZ8', 'price': 2200, 'rest': 5 },
+        { 'id': 103, 'title': 'Nokia 3310', 'price': 500, 'rest': 2 },
+        { 'id': 105, 'title': 'Huawei ZZ', 'price': 1500, 'rest': 8 },
+    ];
 }
