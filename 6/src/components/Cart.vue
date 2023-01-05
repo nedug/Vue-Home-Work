f5da5f1219375eebf5bc6145980f952a9cdf044b
Alexander R <80103497+nedug@users.noreply.github.com>
2023-01-05 13:39:22 +0300

    Update Cart.vue

--- 6/src/components/Cart.vue
+++ 6/src/components/Cart.vue
@@ -9,10 +9,14 @@
 
          <table class="table table-bordered">
             <tr v-for="item in items">
-<!--               <td>{{ item.id }}</td>-->
+               <!--               <td>{{ item.id }}</td>-->
                <td>{{ title(item.id) }}</td>
                <td>{{ price(item.id) }}</td>
                <td>{{ item.cnt }}</td>
+               <td>
+                  <div><button @click="remove(item.id)" type="button" class="btn btn-danger">Remove</button></div>
+               </td>
+               <td><router-link :to="{ name: 'product', params: { id: item.id } }">Read more</router-link></td>
             </tr>
          </table>
       </div>
@@ -24,7 +28,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 
 export default {
@@ -40,6 +44,9 @@ export default {
       price() {
          return id => this.product(id)?.price;
       },
+   },
+   methods: {
+      ...mapActions('cart', ['add', 'remove']),
    },
 };
 </script>
