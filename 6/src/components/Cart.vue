807a0b945f37bd8d8dab3878339b6ec7b00b85cd
Alexander R <80103497+nedug@users.noreply.github.com>
2023-01-05 12:53:12 +0300

    Update Cart.vue

--- 6/src/components/Cart.vue
+++ 6/src/components/Cart.vue
@@ -1,16 +1,14 @@
 <template>
-	<div>
-		<h1>Cart</h1>
-		<hr>
-		<div>table</div>
-		<router-link class="btn btn-success" :to="{ name: 'checkout' }">
-			Order now
-		</router-link>
-	</div>
+   <div>
+      <h1>Cart</h1>
+      <hr>
+      <div>table</div>
+      <router-link class="btn btn-success" :to="{ name: 'checkout' }">
+         Order now
+      </router-link>
+   </div>
 </template>
 
 <script>
-	export default {
-
-	}
+export default {};
 </script>
