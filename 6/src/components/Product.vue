b8aae2b92a8e6a699e66aa2aba9097fe6253b9f4
Alexander R <80103497+nedug@users.noreply.github.com>
2023-01-05 12:10:07 +0300

    Update Product.vue

--- 6/src/components/Product.vue
+++ 6/src/components/Product.vue
@@ -1,15 +1,43 @@
 <template>
    <div>
-      <h1>Title</h1>
-      Back to products
+      <router-link :to="{ name: 'catalog' }">
+         Back to products
+      </router-link>
+
+      <h1>{{ title }}</h1>
+
       <hr>
-      <div class="alert alert-success">
-         price
-         {{ $route.params.id }}
-      </div>
+      <div class="alert alert-success">price: {{ price }}$</div>
    </div>
 </template>
 
 <script>
-export default {};
+import { mapGetters, mapActions } from 'vuex';
+
+
+export default {
+   computed: {
+      ...mapGetters('products', ['product']),
+      ...mapGetters('cart', ['inCart']),
+      id() {
+         return Number(this.$route.params.id);
+      },
+      title() {
+         return this.product(this.id)?.title;
+      },
+      price() {
+         return this.product(this.id)?.price;
+      },
+   },
+   methods: {
+      ...mapActions('cart', ['add', 'remove']),
+   },
+   created() {
+      console.log(!this.product(this.id));
+      if (!this.product(this.id)) {
+         this.$router.push({ name: '404', params: { any: 404 } });
+         // this.$router.push({ name: 'catalog' })
+      }
+   },
+};
 </script>
