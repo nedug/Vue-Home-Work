2b628e73a5ff0fcb82ad56516750f517b418e7fd
Alexander R <80103497+nedug@users.noreply.github.com>
2023-01-11 11:41:01 +0300

    Update Product.vue

--- 7/src/components/Product.vue
+++ 7/src/components/Product.vue
@@ -1,37 +1,49 @@
 <template>
-	<div v-if="hasProduct">
-		<h1>{{ product.title }}</h1>
-		Back to products
-		<hr>
-		<div class="alert alert-success">
-			{{ product.price }}$
-		</div>
-	</div>
-	<app-e-404 v-else />
+   <div v-if="hasProduct">
+      <h1>{{ product.title }}</h1>
+      <router-link :to="{ name: 'catalog' }">Back to products</router-link>
+      <hr>
+      <div class="alert alert-success">
+         price: {{ product.price }}$
+      </div>
+
+      <button v-if="inCart(id)" @click="remove(id)" type="button" class="btn btn-danger">
+         Remove
+      </button>
+      <button v-else @click="add(id)" type="button" class="btn btn-success">
+         Add to cart
+      </button>
+   </div>
+   <app-e-404 v-else />
 </template>
 
 <script>
-	import { mapGetters } from 'vuex';
-	import AppE404 from './E404.vue'
+import { mapGetters, mapActions } from 'vuex';
+import AppE404 from './E404.vue';
 
-	export default {
-		components: {
-			AppE404
-		},
-		computed: {
-			...mapGetters('products', { productById: 'one' }),
-			id(){
-				return parseInt(this.$route.params.id);
-			},
-			validId(){
-				return /^[1-9]+\d*$/.test(this.id);
-			},
-			product(){
-				return this.productById(this.id);
-			},
-			hasProduct(){
-				return this.validId && this.product !== undefined;
-			}
-		}
-	}
+
+export default {
+   components: {
+      AppE404,
+   },
+   computed: {
+      ...mapGetters('products', { productById: 'one' }),
+      ...mapGetters('cart', ['inCart']),
+      id() {
+         return parseInt(this.$route.params.id);
+      },
+      validId() {
+         return /^[1-9]+\d*$/.test(this.id);
+      },
+      product() {
+         return this.productById(this.id);
+      },
+      hasProduct() {
+         return this.validId && this.product !== undefined;
+      },
+   },
+   methods: {
+      ...mapActions('cart', ['add', 'remove']),
+   },
+};
 </script>
