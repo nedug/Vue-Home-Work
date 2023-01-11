443fd479299ec064614f9e7b4492b0899a5884b4
Alexander R <80103497+nedug@users.noreply.github.com>
2023-01-11 15:18:02 +0300

    Update Checkout.vue

--- 7/src/components/Checkout.vue
+++ 7/src/components/Checkout.vue
@@ -1,28 +1,28 @@
 <template>
-	<div>
-		<h1>Your order now</h1>
-		<hr>
+   <div>
+      <h1>Your order now</h1>
+      <hr>
 
-     <div v-if="!length">This will be your last order...</div>
-     <table v-else class="table table-bordered table-hover">
-        <thead>
-        <tr>
-           <th>Title</th>
-           <th>Price</th>
-           <th>Cnt</th>
-           <th>Total</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="pr in order" :key="pr.id">
-           <td>{{ pr.title.trim() }}</td>
-           <td>{{ pr.price }}</td>
-           <td>{{ pr.cnt }}</td>
-           <td>{{ pr.price * pr.cnt }}</td>
-        </tr>
-        </tbody>
-     </table>
-	</div>
+      <div v-if="!length">This will be your last order...</div>
+      <table v-else class="table table-bordered table-hover">
+         <thead>
+         <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Cnt</th>
+            <th>Total</th>
+         </tr>
+         </thead>
+         <tbody>
+         <tr v-for="pr in order" :key="pr.id">
+            <td>{{ pr.title.trim() }}</td>
+            <td>{{ pr.price }}</td>
+            <td>{{ pr.cnt }}</td>
+            <td>{{ pr.price * pr.cnt }}</td>
+         </tr>
+         </tbody>
+      </table>
+   </div>
 </template>
 
 <script>
