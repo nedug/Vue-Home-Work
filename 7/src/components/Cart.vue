74ca20942f19b8c79c1dedb6d540b7ff729b4fce
Alexander R <80103497+nedug@users.noreply.github.com>
2023-01-11 15:16:32 +0300

    Update Cart.vue

--- 7/src/components/Cart.vue
+++ 7/src/components/Cart.vue
@@ -36,22 +36,88 @@
       </table>
       <hr>
       <div v-if="length">
-         <router-link :to="{ name: 'checkout' }" @click="inOrder" class="btn btn-success">Checkout</router-link>
+<!--         <router-link :to="{ name: 'checkout' }" @click="inOrder" class="btn btn-success">Checkout</router-link>-->
+         <button @click="showModal = true" type="button" class="btn btn-success">Checkout</button>
          <button @click="clean" type="button" class="btn btn-danger">Clean Cart</button>
       </div>
    </div>
+
+   <custom-modal
+       v-model="showModal"
+       @confirm="confirm"
+       @cancel="cancel"
+       :transition="{
+      'enter-active-class': 'animate__animated animate__fadeInDown',
+      'leave-active-class': 'animate__animated animate__slideOutUp',
+    }">
+      <template v-slot:title>Are you ready to order?</template>
+      <!--      <table class="table table-bordered">-->
+      <!--         <tbody>-->
+      <!--         <tr v-for="(field, i) in info" :key="i">-->
+      <!--            <td>{{ field.label }}</td>-->
+      <!--            <td>{{ field.value }}</td>-->
+      <!--         </tr>-->
+      <!--         </tbody>-->
+      <!--      </table>-->
+   </custom-modal>
 </template>
+
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import CustomModal from '@/components/CustomModal';
+import '../animate.css';
 
 
 export default {
+   components: {
+      CustomModal,
+   },
+   data: () => ({
+      showModal: false,
+   }),
    computed: {
       ...mapGetters('cart', { products: 'itemsDetailed', length: 'length' }),
    },
    methods: {
       ...mapActions('cart', ['setCnt', 'remove', 'clean', 'inOrder']),
+      confirm() {
+         this.inOrder();
+         this.$router.push({ name: 'checkout'});
+         this.showModal = false;
+      },
+      cancel(close) {
+         close();
+      },
    },
 };
 </script>
+
+<style>
+
+.fade-enter-active {
+   animation: iconIn .3s;
+}
+
+.fade-leave-active {
+   animation: iconOut .3s;
+}
+
+@keyframes iconIn {
+   from {
+      transform: translateY(-100px);
+   }
+   to {
+      transform: translateY(0);
+   }
+}
+
+@keyframes iconOut {
+   from {
+      transform: translateY(0)
+   }
+   to {
+      transform: translateY(100px);
+   }
+}
+</style>
