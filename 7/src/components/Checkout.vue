4e983170cc5f281cb40d8e6c837d43ff96434d3c
Alexander R <80103497+nedug@users.noreply.github.com>
2023-01-11 13:58:35 +0300

    Update Checkout.vue

--- 7/src/components/Checkout.vue
+++ 7/src/components/Checkout.vue
@@ -2,11 +2,36 @@
 	<div>
 		<h1>Your order now</h1>
 		<hr>
+
+     <div v-if="!length">This will be your first order...</div>
+     <table v-else class="table table-bordered table-hover">
+        <thead>
+        <tr>
+           <th>Title</th>
+           <th>Price</th>
+           <th>Cnt</th>
+           <th>Total</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="pr in order" :key="pr.id">
+           <td>{{ pr.title.trim() }}</td>
+           <td>{{ pr.price }}</td>
+           <td>{{ pr.cnt }}</td>
+           <td>{{ pr.price * pr.cnt }}</td>
+        </tr>
+        </tbody>
+     </table>
 	</div>
 </template>
 
 <script>
-	export default {
-		
-	}
+import { mapGetters } from 'vuex';
+
+
+export default {
+   computed: {
+      ...mapGetters('cart', { products: 'itemsDetailed', length: 'lengthOrder', order: 'order' }),
+   },
+};
 </script>
