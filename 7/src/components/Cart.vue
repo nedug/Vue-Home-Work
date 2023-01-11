55b7f4e996494b20d33078f22c09990d6db42e56
Alexander R <80103497+nedug@users.noreply.github.com>
2023-01-11 11:58:00 +0300

    Update Cart.vue

--- 7/src/components/Cart.vue
+++ 7/src/components/Cart.vue
@@ -1,44 +1,52 @@
 <template>
-	<div>
-		<h1>Cart</h1>
-		<hr>
-		<table class="table table-bordered table-hover">
-			<thead>
-				<tr>
-					<th>Title</th>
-					<th>Price</th>
-					<th>Cnt</th>
-					<th>Total</th>
-					<th>Actions</th>
-				</tr>
-			</thead>	
-			<tbody>
-				<tr v-for="pr in products" :key="pr.id">
-					<td>{{ pr.title.trim() }}</td>
-					<td>{{ pr.price }}</td>
-					<td>{{ pr.cnt }}</td>
-					<td>{{ pr.price * pr.cnt }}</td>
-					<td>
-						<button class="btn btn-warning me-1" @click="setCnt({ id: pr.id, cnt: pr.cnt - 1 })">-1</button>
-						<button class="btn btn-success" @click="setCnt({ id: pr.id, cnt: pr.cnt + 1 })">+1</button>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-		<hr>
-		<router-link :to="{ name: 'checkout' }" class="btn btn-success">Checkout</router-link>
-	</div>
+   <div>
+      <h1>Cart</h1>
+      <hr>
+      <div v-if="!length">Корзина пуста...</div>
+      <table v-else class="table table-bordered table-hover">
+         <thead>
+         <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Cnt</th>
+            <th>Total</th>
+            <th>Actions</th>
+            <th>Remove</th>
+            <th>Link</th>
+         </tr>
+         </thead>
+         <tbody>
+         <tr v-for="pr in products" :key="pr.id">
+            <td>{{ pr.title.trim() }}</td>
+            <td>{{ pr.price }}</td>
+            <td>{{ pr.cnt }}</td>
+            <td>{{ pr.price * pr.cnt }}</td>
+            <td>
+               <button class="btn btn-warning me-1" @click="setCnt({ id: pr.id, cnt: pr.cnt - 1 })">-1</button>
+               <button class="btn btn-success" @click="setCnt({ id: pr.id, cnt: pr.cnt + 1 })">+1</button>
+            </td>
+            <td> <button @click="remove(pr.id)" type="button" class="btn btn-danger">x</button></td>
+            <td>
+               <router-link :to="{ name: 'product', params: { id: pr.id } }">Read more</router-link>
+            </td>
+         </tr>
+         </tbody>
+      </table>
+      <hr>
+      <router-link v-if="length" :to="{ name: 'checkout' }" class="btn btn-success">Checkout</router-link>
+   </div>
 </template>
 
 <script>
-	import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
-	export default {
-		computed: {
-			...mapGetters('cart', { products: 'itemsDetailed' })
-		},
-		methods: {
-			...mapActions('cart', [ 'setCnt' ])
-		}
-	}
+
+export default {
+   computed: {
+      ...mapGetters('cart', { products: 'itemsDetailed', length: 'length' }),
+   },
+   methods: {
+      ...mapActions('cart', ['setCnt', 'remove',]),
+   },
+};
 </script>
